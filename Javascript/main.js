bb081ce9fa0fc50b19ac7c5d7f91adf986331940
Rodro-Kumar <rudrokormokar96@gmail.com>
2023-12-27 13:31:15 +0600

    major update

--- Javascript/main.js
+++ Javascript/main.js
@@ -7,10 +7,24 @@ const lowercase = uppercase.toLowerCase();
 const number = "1234567890";
 const specialCha = "!@#$%^&*+_.?-";
 
+const allItem = uppercase + lowercase + number + specialCha;
+const passwordLength = 10;
+
 generateBtn.addEventListener("click", function () {
   const upper = uppercase[Math.floor(Math.random() * uppercase.length)];
   const lower = lowercase[Math.floor(Math.random() * lowercase.length)];
   const num = number[Math.floor(Math.random() * number.length)];
   const special = specialCha[Math.floor(Math.random() * specialCha.length)];
-  console.log(upper + lower + special + num);
+  let password = upper + lower + special + num;
+  while (password.length <= passwordLength) {
+    password += allItem[Math.floor(Math.random() * allItem.length)];
+  }
+  input.value = password;
+});
+
+copyBtn.addEventListener("click", function () {
+  navigator.clipboard.writeText(input.value);
+  copyBtn.innerHTML = "Copied";
+  input.value = "copied";
+  input.style.color = "#FF9800";
 });
